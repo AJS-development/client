38858742cd22e182f02c5bd39ffc853d52c98f17
Andrew54757 <andrews54757@gmail.com>
2016-05-20 22:53:03 -0400

    Update main_out.js

--- main_out.js
+++ main_out.js
@@ -611,7 +611,7 @@
     }
 
     function drawChatBoard() {
-        chatCanvas = null;
+       // chatCanvas = null;
         chatCanvas = document.createElement("canvas");
         var ctx = chatCanvas.getContext("2d");
         var scaleFactor = Math.min(Math.max(canvasWidth / 1200, 0.75),1); //scale factor = 0.75 to 1
